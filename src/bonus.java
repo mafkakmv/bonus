c37e04b399a9ce045c7be2910d02633de408420b
Mafka <kitm@digt.ru>
2023-05-06 18:23:48 +0300

    bonus

---
+++ src/bonus.java
@@ -0,0 +1,18 @@
+public class bonus {
+    public static void main(String[] args) {
+
+        int customerAccount = 100;
+        int amountReplenishment = 1100;
+
+        int finalScore;
+        int numberBonus;
+
+        if ( amountReplenishment > 1000) {
+            System.out.println( numberBonus = amountReplenishment / 100);
+            System.out.println( finalScore = customerAccount + amountReplenishment + numberBonus);
+        } else {
+            System.out.println( numberBonus = 0);
+            System.out.println( finalScore = customerAccount + amountReplenishment);
+        }
+    }
+}
